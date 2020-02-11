a514c2cdbe9197006f34592ef512348e94d7ed05
Leo <lsegars@gmail.com>
2020-02-11 07:54:49 -0300

    Video 7

--- 1.Hola Mundo/src/app/app.component.ts
+++ 1.Hola Mundo/src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{name}}</h1>`,
+  templateUrl: 'app-component.html',
 })
 export class AppComponent  { name = 'Hola Mundo'; }
