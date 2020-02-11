d6c3f1df284c30a960a67f9d5e33d5beee2b9dd6
Leonardo SEGARS <SegarsL@sancristobal.com.ar>
2020-02-11 11:28:50 -0300

    Video 8

--- 1.Hola Mundo/src/app/components/header.component.ts
+++ 1.Hola Mundo/src/app/components/header.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-header',
-  template: `<h1>{{name}}</h1>`,
+  templateUrl: 'header.component.html'
 })
-export class HeaderComponent  { name = 'Hola Mundo'; }
+export class HeaderComponent  {  }
