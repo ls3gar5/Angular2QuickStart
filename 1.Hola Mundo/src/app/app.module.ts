a6dbce622ec005021d8eb2c2b548e6cde5cc6b38
Leo <lsegars@gmail.com>
2020-02-12 07:16:57 -0300

    Footer

--- 1.Hola Mundo/src/app/app.module.ts
+++ 1.Hola Mundo/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { HeaderComponent }  from './components/header.component';
 import { BodyComponent } from './components/body.component';
+import { FooterComponent } from './components/footer.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ AppComponent, 
                   HeaderComponent,
-                  BodyComponent ],
+                  BodyComponent,
+                  FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- 1.Hola Mundo/src/app/components/body.component.ts
+++ 1.Hola Mundo/src/app/components/body.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
     selector: 'app-body',
-    templateUrl: 'body.component.html',
+    templateUrl: './body.component.html',
 })
 export class BodyComponent {
     
