5f84428789092e8bdf24f72c3ffa8faf8f3a591d
Leo <lsegars@gmail.com>
2020-02-13 08:32:57 -0300

    Video 11

--- 1.Hola Mundo/src/app/components/body.component.ts
+++ 1.Hola Mundo/src/app/components/body.component.ts
@@ -5,5 +5,9 @@ import { Component } from '@angular/core';
     templateUrl: './body.component.html',
 })
 export class BodyComponent {
-    
+    mostrar: boolean = false;
+    texto: string = "Un gran poder, requiere una gran responsabilidad";
+    autor: string = "Ben Parker";
+
+    personajes: string[] = ["Spiderman", "Venom", "Octupus"];
 }
